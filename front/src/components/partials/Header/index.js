caef3b92980a15efc3205028773ba9df76b0f276
angeloalf <angelo.alf@gmail.com>
2021-09-18 16:36:07 -0300

    Etapa 6 - Criando o Header (estrutura e logo)

--- front/src/components/partials/Header/index.js
+++ front/src/components/partials/Header/index.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {Link} from 'react-router-dom' // component Link para a raiz (home)
 
 import {HeaderArea} from './styled' 
 
 function Header() {
     return (<>
         <HeaderArea>
-            ........... header
+            <div className="container">
+                <div className="logo">
+                    <Link to="/">
+                        <span className="logoO">O</span>
+                        <span className="logoL">L</span>
+                        <span className="logoX">X</span>
+                    </Link>
+
+                </div>
+            </div>
         </HeaderArea>
     
     </>
--- front/src/components/partials/Header/styled.js
+++ front/src/components/partials/Header/styled.js
@@ -1,7 +1,37 @@
 import styled from "styled-components" // pegar da dependência
 
 export const HeaderArea = styled.div`
-    height: 100px;
-    background-color: #CCC;
+background-color: #fff;
+height: 60px;
+border-bottom: 1px solid #ccc;
 
+a {
+    text-decoration: none;
+}
+
+.container {
+    max-width: 1000px;
+    margin: auto;
+    display: flex;
+}
+
+.logo {
+    flex: 1;
+    display: flex;
+    align-itens: center;
+    height: 60px;
+    line-height: 60px;
+}
+
+.logoO, 
+.logoL,
+.logoX {
+    font-size: 30px;
+    font-weight: bold;
+}
+
+.logoO  {color: #FF0000;}
+.logoL  {color: #00FF00;}
+.logoX  {color: #0000FF;}
+    
 `
